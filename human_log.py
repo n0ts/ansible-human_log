ba77f303a6683d8dab55ec22fa628ba026952dfc
Tobias Trabelsi <lerentis@burntbunch.org>
2017-10-14 01:01:16 +0200

    fixed deprecated use of CallbackBase

--- human_log.py
+++ human_log.py
@@ -17,6 +17,8 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+from ansible.plugins.callback import CallbackBase
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -31,7 +33,7 @@ FIELDS = ['cmd', 'command', 'start', 'end', 'delta', 'msg', 'stdout',
           'stderr', 'results']
 
 
-class CallbackModule(object):
+class CallbackModule(CallbackBase):
 
     """
     Ansible callback plugin for human-readable result logging
